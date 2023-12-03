4ce3437b51b8e75538d010fc1269ee78984b17ab
Jozef Mikus <jozef.mikus@kyndryl.com>
2023-12-03 10:49:46 +0100

    1.1 done

---
+++ src/main/kotlin/advent23/d1/trebuchet/FirstLastDigit.kt
@@ -0,0 +1,47 @@
+package advent23.d1.trebuchet
+
+import java.nio.file.Path
+import kotlin.io.path.bufferedReader
+
+var result = 0
+
+fun main() {
+    println("Current working directory: ${System.getProperty("user.dir")}")
+    Path.of("src/main/kotlin/advent23/d1/trebuchet/in.txt")
+        .bufferedReader()
+        .useLines { lines -> lines.forEach {
+            result += twoDigitNumberFromFirstLastDigit(it)
+        }
+    }
+    println("Result is: $result")
+}
+
+fun twoDigitNumberFromFirstLastDigit(line: String): Int {
+    if (line.isEmpty()) {
+        return 0
+    }
+    if (line.length == 1) {
+        return if (line[0].isDigit()) line[0].digitToInt() else 0
+    }
+
+    var result = 0
+    var frontStopper = false
+    var endIdx = line.length -1
+    var endStopper = false
+
+    for (frontIdx in line.indices) {
+        if (frontStopper && endStopper) {
+            break
+        }
+        if (line[frontIdx].isDigit() && !frontStopper) {
+            result += 10 * line[frontIdx].digitToInt()
+            frontStopper = true
+        }
+        if (line[endIdx].isDigit() && !endStopper) {
+            result += line[endIdx].digitToInt()
+            endStopper = true
+        }
+        endIdx -= 1
+    }
+    return result
+}
---
+++ src/test/kotlin/advent23/d1/trebuchet/FirstLastDigitKtTest.kt
@@ -0,0 +1,5 @@
+package advent23.d1.trebuchet
+
+import org.junit.jupiter.api.Assertions.*
+
+class FirstLastDigitKtTest
